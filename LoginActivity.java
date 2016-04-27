07ce67ae0ccf9c24ca64a6bf6511b640f1ac86f3
Mridul-Xpetize <mridul@xpetize.com>
2016-04-27 17:50:21 +0530

    Added methods for decryption

--- LoginActivity.java
+++ LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,8 +16,22 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+
 public class LoginActivity extends AppCompatActivity {
 
+    private static String cryptoPass = "sup3rS3yx";
     Button login;
     EditText username, password;
     String username_st, password_st;
@@ -28,6 +43,7 @@ public class LoginActivity extends AppCompatActivity {
     private static String TAG_ID = "Id";
     private static String TAG_DESIGNATION = "Designation";
     private static String TAG_USERNAME = "UserName";
+    String decryptedUsername, decryptedPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -125,6 +141,39 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
+    public static String decryptIt(String value) {
+        try {
+            DESKeySpec keySpec = new DESKeySpec(cryptoPass.getBytes("UTF8"));
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+            SecretKey key = keyFactory.generateSecret(keySpec);
+
+            byte[] encrypedPwdBytes = Base64.decode(value, Base64.DEFAULT);
+            // cipher is not thread safe
+            Cipher cipher = Cipher.getInstance("DES");
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] decrypedValueBytes = (cipher.doFinal(encrypedPwdBytes));
+
+            String decrypedValue = new String(decrypedValueBytes);
+            Log.d("TAG", "Decrypted: " + value + " -> " + decrypedValue);
+            return decrypedValue;
+
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+        return value;
+    }
 
     @Override
     protected void onPause() {
